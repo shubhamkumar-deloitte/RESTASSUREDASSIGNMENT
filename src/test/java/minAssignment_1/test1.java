b9827c2fb9ebffe5f918c532903feb45bf2b7bb0
shubhamkumar-deloitte <shubhamkumar-deloitte@users.noreply.github.com>
2022-03-31 12:23:32 +0530

    commiting whole assignment due to some error

---
+++ src/test/java/minAssignment_1/test1.java
@@ -0,0 +1,43 @@
+package minAssignment_1;
+
+import org.testng.annotations.Test;
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+import org.testng.Assert;
+
+import java.io.File;
+public class test1 {
+
+    @Test
+    public void test_get_call(){
+
+        given().
+                baseUri("https://jsonplaceholder.typicode.com/posts").header("Content-Type","application/json").
+                when().
+                get("https://jsonplaceholder.typicode.com/posts").
+                then().
+                statusCode(200).body("userId[39]",equalTo(4)).body("title[39]",equalTo("enim quo cumque"));
+
+
+    }
+    @Test
+    public void test_put_call(){
+        File JsonData=new File("C:\\Users\\shubhamkumar32\\IdeaProjects\\restAssured\\src\\test\\java\\resources\\putData.json");
+        given().
+                baseUri("https://reqres.in/api/users").
+                body(JsonData).
+                header("Content-Type","application/json").
+                when().
+                put("/users").
+                then().
+                statusCode(200).body("name",equalTo("Arun")).body("job",equalTo("Manager"));
+
+
+
+
+    }
+}
