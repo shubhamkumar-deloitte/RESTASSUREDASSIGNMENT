57af0676b832db3361c150f7579a903f442a476f
shubhamkumar-deloitte <shubhamkumar-deloitte@users.noreply.github.com>
2022-03-31 13:30:05 +0530

    fixing my pom.exl dependency issues and updating the code

--- src/test/java/minAssignment_1/test1.java
+++ src/test/java/minAssignment_1/test1.java
@@ -1,40 +1,55 @@
 package minAssignment_1;
-
-import org.testng.annotations.Test;
 import io.restassured.RestAssured;
-import io.restassured.path.json.JsonPath;
-
-import static io.restassured.RestAssured.*;
-import static org.hamcrest.Matchers.*;
-
-import org.testng.Assert;
+import io.restassured.response.Response;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 import java.io.File;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+
+
 public class test1 {
 
-    @Test
+    @Test(priority = 1)
     public void test_get_call(){
 
-        given().
-                baseUri("https://jsonplaceholder.typicode.com/posts").header("Content-Type","application/json").
-                when().
-                get("https://jsonplaceholder.typicode.com/posts").
-                then().
-                statusCode(200).body("userId[39]",equalTo(4)).body("title[39]",equalTo("enim quo cumque"));
+//        given().
+//                baseUri("https://jsonplaceholder.typicode.com/posts").header("Content-Type","application/json").
+//                when().
+//                get("https://jsonplaceholder.typicode.com/posts").
+//                then().
+//                statusCode(200).body("userId[39]",equalTo(4)).body("title[39]",equalTo("enim quo cumque"));
+
+        Response response=given().when().get("https://jsonplaceholder.typicode.com/posts").then().extract().response();
+        JSONArray arr=new JSONArray(response.asString());
+        for (int i = 0; i < arr.length(); i++) {
+            JSONObject obj = arr.getJSONObject(i);
+            assert obj.getInt("userId") != 40 || (obj.getInt("id") == 4);
+            assert obj.get("title") != null;
+            assert obj.get("title") instanceof String;
+        }
+
+        assert (response.getStatusCode()==200);
+        assert(response.getContentType().contains("json"));
+
 
 
     }
-    @Test
+    @Test(priority = 2)
     public void test_put_call(){
+
         File JsonData=new File("C:\\Users\\shubhamkumar32\\IdeaProjects\\restAssured\\src\\test\\java\\resources\\putData.json");
-        given().
-                baseUri("https://reqres.in/api/users").
-                body(JsonData).
-                header("Content-Type","application/json").
-                when().
-                put("/users").
-                then().
-                statusCode(200).body("name",equalTo("Arun")).body("job",equalTo("Manager"));
+        Response response = given().baseUri("https://reqres.in/api").body(JsonData).header("Content-Type", "application/json").
+                when().put("/users").then().extract().response();
+        assert response.statusCode() == 200;
+        JSONObject obj = new JSONObject(response.asString());
+        assert obj.get("name").equals("Arun") && obj.get("job").equals("Manager");
 
 
 
