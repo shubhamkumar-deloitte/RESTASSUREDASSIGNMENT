c9ea14054ea1996974b5b5f59bed0c306998826d
shubhamkumar-deloitte <shubhamkumar-deloitte@users.noreply.github.com>
2022-04-04 01:27:44 +0530

    all tasks activities added adding tasks, validating page limit, validating tasks data

---
+++ src/test/java/MainAssignment/activities/taskActivities.java
@@ -0,0 +1,113 @@
+package MainAssignment.activities;
+
+import MainAssignment.registrationUtils.readingFromExcel;
+import org.json.JSONArray;
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import org.apache.logging.log4j.Logger;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+
+public class taskActivities  extends loginUser{
+
+
+    public taskActivities(String url, Logger logger) {
+        super(url, logger);
+    }
+
+    RequestSpecification requestSpecification;
+    ResponseSpecification responseSpecification;
+    //list to contain all the tasks
+    List<String>allTasks;
+
+
+
+    public void setup(){
+        RequestSpecBuilder requestSpecBuilder=new RequestSpecBuilder();
+        requestSpecBuilder.setBaseUri(url).addHeader("Content-Type","application/json")
+                .addHeader("Authorization","Bearer "+token);
+
+        requestSpecification= RestAssured.with().spec(requestSpecBuilder.build());
+
+        ResponseSpecBuilder responseSpecBuilder=new ResponseSpecBuilder();
+        responseSpecBuilder.expectContentType(ContentType.JSON);
+        responseSpecification=responseSpecBuilder.build();
+
+
+    }
+    //copying all the list into this list
+    public void extractAllTasks() throws IOException {
+        allTasks= readingFromExcel.readAllTasksFromExcel();
+    }
+
+    //method to add a particular task
+
+    public void addOnetask(JSONObject object){
+        logger.info("task being added",object);
+
+        Response response=given().spec(requestSpecification).body(object.toString()).when().post("/task")
+                .then().extract().response();
+
+        assert response.statusCode()==201;
+
+        logger.info("task added");
+    }
+
+    //now loop over the list and add task one by one
+    public void addAllTasks() throws IOException {
+        extractAllTasks();
+
+        //looping over the list of type string
+        for(String task:allTasks){
+            addOnetask(new JSONObject().put("description",task));
+        }
+
+        logger.info("all tasks added successfully");
+    }
+
+    //validating number of tasks shown when page limit is entered
+
+    public void numberOfTasksShown(int pageLimit){
+        Response response=given().spec(requestSpecification).param("limit",pageLimit).when().get("/task").then().spec(responseSpecification)
+                .extract().response();
+
+        JSONObject object=new JSONObject(response.asString());
+        //to count the number of tasks shown when page limit is applied
+        JSONArray jsonArray=new JSONArray(object.getJSONArray("data"));
+
+        assert pageLimit==jsonArray.length();
+
+    }
+
+    //validate the owner and description of all the tasks shown
+
+    public boolean  validateAllTasksInfo(){
+        Response response=given().spec(requestSpecification).when().get("/task").then().spec(responseSpecification)
+                .extract().response();
+
+        JSONObject object=new JSONObject(response.asString());
+        JSONArray jsonArray=new JSONArray(object.getJSONArray("data"));
+
+        String owner=jsonArray.getJSONObject(0).getString("owner");
+        for(int i=0;i<jsonArray.length();i++){
+            JSONObject object1=jsonArray.getJSONObject(i);
+            if(!allTasks.get(i).equals(object1.getString("description")) || !owner.equals(object1.getString("owner"))){
+                return false;
+            }
+        }
+
+        return true;
+
+
+
+    }
+}
---
+++ src/test/java/MainAssignment/testClass.java
@@ -0,0 +1,10 @@
+package MainAssignment;
+
+import org.apache.logging.log4j.Logger;
+
+public class testClass {
+
+    Logger logger=baseClass.log;
+
+
+}
